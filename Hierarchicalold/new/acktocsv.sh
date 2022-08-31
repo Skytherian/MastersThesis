d8dc7d09695da0c8b5316c0076dcf3d2772ccf69
Skytherian <scyther@localhost.localdomain>
2022-08-31 11:37:27 +0530

    Remove

--- Hierarchicalold/new/acktocsv.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
- ack ARI -C 1 | perl -pe 's#^.*?/##g'| perl -pe 's/\s+.Txt//g'| perl -pe 's/(:|-)\d(:|-)//g' | perl -pe 's/^--\n//g' | sed '/ARI/d'| perl -pe 's/Distance/Distance,/g'| perl -pe 's/(\d\.\d+)/$1,/g' | perl -pe 's/,(?=$)//g' > complete.csv
-
